94ac97515f27136dd7a7888d409f1ed3e3752389
nelson-lz <nelsonlz@outlook.com>
2020-03-24 22:26:49 -0400

    Back-end de la aplicacion

---
+++ backend/src/controllers/OngController.js
@@ -0,0 +1,28 @@
+const connection = require('../database/connection');
+const crypto = require('crypto');
+module.exports = {
+    async index (request, response){
+        const ongs = await connection('ongs').select('*');
+        
+        return response.json(ongs);
+    },
+    
+    async create (request, response) {
+        const {name, email, city, whatsapp, uf} = request.body;
+        const id = crypto.randomBytes(4).toString('HEX');
+    
+        await connection('ongs').insert({
+            id,
+            name,
+            email,
+            city,
+            whatsapp,
+            uf,
+        });
+        
+        return response.json({id});
+    }
+
+
+
+}
